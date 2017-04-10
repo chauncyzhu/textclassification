2b8cb05714ab5148ce8e5a21bd066585b8668144
chauncyzhu <chauncyzhu@163.com>
2017-04-10 19:56:00 +0800

    修正了main函数调用

--- classification/knn/main.py
+++ classification/knn/main.py
@@ -1,12 +1,13 @@
 # coding=gbk
 import pandas as pd
-import knn
+from classification.knn.knn import *
 """
     这里是KNN的调用函数
 """
 train_csv = "../../data/reuters/reuter_train.csv"  #分开的训练集
 test_csv = "../../data/reuters/reuter_test.csv"   #分开的测试集
-k_list = [50,60,70,80,90,100]
+evaluation_csv = "../../data/reuters/evaluation_csv.csv"   #结果集
+k_list = [1,2,3,4,5,6,7,8,9,10]
 class_num = 8
 
 
@@ -19,6 +20,7 @@ pd_test = pd_test.head(50)
 
 if __name__ == '__main__':
     #调用KNN
-    result = knn.KNN(pd_train,pd_test,k_list)
-    evaluation_result = knn.evaluation(result,class_num,k_list)
+    result = KNN(pd_train,pd_test,k_list)
+    evaluation_result = pd.DataFrame(evaluation(result,class_num,k_list),columns=['macro_f1','micro_f1'])
+    evaluation_result.to_csv(evaluation_csv)
     print(evaluation_result)
