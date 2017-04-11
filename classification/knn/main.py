01054ccae5b663b70b2c4dbeb44de1309eb5e864
chauncyzhu <chauncyzhu@163.com>
2017-04-11 21:06:48 +0800

    knn调用函数，分别进行多分类和二分类

--- classification/knn/main.py
+++ classification/knn/main.py
@@ -6,38 +6,41 @@ from classification.knn.evaluation import *
     这里是KNN的调用函数
 """
 #默认参数
-TRAIN_CSV = "../../data/reuters/reuter_train.csv"  #分开的训练集
-TEST_CSV = "../../data/reuters/reuter_test.csv"   #分开的测试集
-EVALUATION_MULTI_CSV = "../../data/reuters/evaluation_multi_csv.csv"   #多分类结果集
-EVALUATION_BINARY_CSV = "../../data/reuters/evaluation_binary_csv.csv"   #二分类结果集
+EVALUATION_MULTI_BDC_CSV = "../../data/reuters/evaluation_multi_csv.csv"   #多分类结果集
+EVALUATION_BINARY_DF_BDC_CSV = "../../data/reuters/evaluation_binary_csv.csv"   #二分类结果集
 K_LIST = [50,60,70,80,90,100]
 CLASS_NUM = 8   #多分类类的个数
 CONFIRM_POS_CLASS = 0  #指定二分类正类序号
 
-#数据读取
-PD_TRAIN = pd.read_csv(TRAIN_CSV)
-PD_TEST = pd.read_csv(TEST_CSV)
-
-#数据量控制
-PD_TEST = PD_TEST.head(50)
-
 #获得多分类数据
-def getMultiClassData():
-    return (PD_TRAIN,PD_TEST)  #后面其实没有对数据作改动
+def get_multi_class_data():
+    train_bdc_csv = "../data/reuters/multiclass/reuter_train_multiclass_bdc.csv"  # 分开的训练集
+    test_bdc_csv = "../data/reuters/multiclass/reuter_test_multiclass_bdc.csv"  # 分开的测试集
+    train_df_bdc_csv = "../data/reuters/multiclass/reuter_train_multiclass_df_bdc.csv"  # 分开的训练集
+    test_df_bdc_csv = "../data/reuters/multiclass/reuter_test_multiclass_df_bdc.csv"  # 分开的测试集
+
+    # 数据读取
+    pd_train = pd.read_csv(train_bdc_csv)
+    pd_test = pd.read_csv(test_bdc_csv)
+
+    # 数据量控制
+    pd_test = pd_test.head(50)
+    return (pd_train,pd_test)  #后面其实没有对数据作改动
 
 #获得二分类数据，只需要改动class列就行
-def getBinaryClassData():
-    def f(x):
-        if eval(x)[CONFIRM_POS_CLASS] == 1:
-            return '[1,0]'  #属于正类
-        else:
-            return '[0,1]'  #属于负类
-    PD_TRAIN['class'] = PD_TRAIN['class'].apply(f)  #apply返回的是一个新的Dataframe
-    PD_TEST['class'] = PD_TEST['class'].apply(f)
-    print(PD_TRAIN)
-    print(PD_TEST)
-    return (PD_TRAIN,PD_TEST)
+def get_binary_class_data():
+    train_bdc_csv = "../data/reuters/binaryclass/reuter_train_binaryclass_bdc.csv"  # 分开的训练集
+    test_bdc_csv = "../data/reuters/binaryclass/reuter_test_binaryclass_bdc.csv"  # 分开的测试集
+    train_df_bdc_csv = "../data/reuters/binaryclass/reuter_train_binaryclass_df_bdc.csv"  # 分开的训练集
+    test_df_bdc_csv = "../data/reuters/binaryclass/reuter_test_binaryclass_df_bdc.csv"  # 分开的测试集
 
+    # 数据读取
+    pd_train = pd.read_csv(train_bdc_csv)
+    pd_test = pd.read_csv(test_bdc_csv)
+
+    # 数据量控制
+    pd_test = pd_test.head(50)
+    return (pd_train,pd_test)
 
 if __name__ == '__main__':
     #调用KNN
@@ -48,8 +51,8 @@ if __name__ == '__main__':
     # evaluation_multi_result.to_csv(EVALUATION_MULTI_CSV)
 
     #获得二分类的数据
-    pd_binary_train, pd_binary_test = getBinaryClassData()
+    pd_binary_train, pd_binary_test = get_binary_class_data()
     binary_result = knn(pd_binary_train, pd_binary_test, K_LIST)
     evaluation_binary_result = pd.DataFrame(evaluation_binaryclass(binary_result, K_LIST), columns=['precision','recall','f1'])
-    evaluation_binary_result.to_csv(EVALUATION_BINARY_CSV)
+    evaluation_binary_result.to_csv(EVALUATION_MULTI_BDC_CSV)
     print("evaluation_binary_result:",evaluation_binary_result)
