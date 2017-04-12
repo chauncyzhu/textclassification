08f2ac26f7421b4fde0bb9d5271ed4a02d322cad
chauncyzhu <chauncyzhu@163.com>
2017-04-12 14:51:51 +0800

    修改一点错误，应该是在textclassificatoin文件目录下

--- utils/newsgroup_path.py
+++ utils/newsgroup_path.py
@@ -4,6 +4,23 @@
 """
 ROOT_PATH = "D:/Coding/pycharm-professional/pycharm-file/pythondata/"
 
-NEWSGROUP = ROOT_PATH + "20newsgroup"  #newsgroup
+NEWSGROUP = ROOT_PATH + "newsgroup/textclassification"  #newsgroup
 
-SOURCEFILE = ROOT_PATH + "20newsgroup/6ng.txt"  # 路透社语料库
+SOURCEFILE = ROOT_PATH + "newsgroup/6ng.txt"  # 路透社语料库
+
+VOCA_MULTI_CSV = NEWSGROUP + "/multiclass/voca_dict_multiclass.csv"  # 字典
+VOCA_BINARY_CSV = NEWSGROUP + "/binaryclass/voca_dict_binaryclass.csv"  # 字典
+
+TRAIN_MULTI_BDC_CSV = NEWSGROUP + "/multiclass/newsgroup_train_multiclass_bdc.csv"  # 分开的训练集
+TEST_MULTI_BDC_CSV = NEWSGROUP + "/multiclass/newsgroup_test_multiclass_bdc.csv"  # 分开的测试集
+TRAIN_MULTI_DF_BDC_CSV = NEWSGROUP + "/multiclass/newsgroup_train_multiclass_df_bdc.csv"  # 分开的训练集
+TEST_MULTI_DF_BDC_CSV = NEWSGROUP + "/multiclass/newsgroup_test_multiclass_df_bdc.csv"  # 分开的测试集
+
+TRAIN_BINARY_BDC_CSV = NEWSGROUP + "/binaryclass/newsgroup_train_binaryclass_bdc.csv"  # 分开的训练集
+TEST_BINARY_BDC_CSV = NEWSGROUP + "/binaryclass/newsgroup_test_binaryclass_bdc.csv"  # 分开的测试集
+TRAIN_BINARY_DF_BDC_CSV = NEWSGROUP + "/binaryclass/newsgroup_train_binaryclass_df_bdc.csv"  # 分开的训练集
+TEST_BINARY_DF_BDC_CSV = NEWSGROUP + "/binaryclass/newsgroup_test_binaryclass_df_bdc.csv"  # 分开的测试集
+
+EVALUATION_BIANRY_BDC_CSV = NEWSGROUP + "/evaluation/evaluation_binary_bdc_csv.csv"  # 多分类结果集
+EVALUATION_BINARY_DF_BDC_CSV = NEWSGROUP + "/evaluation/evaluation_binary_df_bdc_csv.csv"  # 二分类结果集
+
