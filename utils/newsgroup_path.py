9f53095d53e634f732b24d6ee545a6521285c053
chauncyzhu <chauncyzhu@163.com>
2017-04-12 13:14:33 +0800

    20newsgroup的数据路径

---
+++ utils/newsgroup_path.py
@@ -0,0 +1,9 @@
+# coding=gbk
+"""
+    存放各个数据文件的文件路径，因为文件过大不好上传到github上
+"""
+ROOT_PATH = "D:/Coding/pycharm-professional/pycharm-file/pythondata/"
+
+NEWSGROUP = ROOT_PATH + "20newsgroup"  #newsgroup
+
+SOURCEFILE = ROOT_PATH + "20newsgroup/6ng.txt"  # 路透社语料库
